c3b5bbf853865182d8b7907896a8d754a0fd9b5b
jijiahui <847526323@qq.com>
2021-11-05 18:16:27 +0800

    光源

--- shader.fs
+++ shader.fs
@@ -4,14 +4,20 @@ out vec4 FragColor;
 struct Material{
     sampler2D diffuse;
     sampler2D specular;
+    sampler2D creativesam;
     float shininess;
 };
 
 struct Light{
     vec3 position;
+    vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 uniform Light light;
@@ -26,6 +32,9 @@ uniform vec3 viewPos;
 
 void main()
 {
+    float distance  = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+
     vec3 ambient = vec3(texture(material.diffuse,TexCoords)) * light.ambient;
 
     vec3 norm = normalize(Normal);
@@ -36,7 +45,8 @@ void main()
     vec3 viewDir = normalize(viewPos-FragPos);
     vec3 reflectDir = reflect(-lightDir,norm);
     float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
-    vec3 specular = vec3(texture(material.specular,TexCoords)) * spec * light.specular;
+    vec3 specular = (texture(material.specular,TexCoords).rgb) * spec * light.specular;
+    vec3 creativesam = texture(material.creativesam,TexCoords).rgb;
 
-    FragColor = vec4((ambient+diffuse+specular),1.0);
+    FragColor = vec4((ambient+diffuse+specular)*attenuation,1.0);
 }
